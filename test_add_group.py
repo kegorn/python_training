6bae33c3cfaedfdfd8a148aae42c57e2753c8a10
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-13 10:27:51 +0200

    1.6 Introduction parameter object
    Added Group object - parameter object

--- test_add_group.py
+++ test_add_group.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 
 import unittest
+from group import Group
 
 
 class test_add_group(unittest.TestCase):
@@ -20,7 +21,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
         self.open_groups_page(driver)
-        self.create_group(driver, name="new_group1", header="new_group1", footer="new_group1")
+        self.create_group(driver, Group(name="new_group1", header="new_group1", footer="new_group1"))
         self.return_to_groups_page(driver)
         self.logout(driver)
 
@@ -29,7 +30,7 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(driver)
         self.login(driver, username="admin", password="secret")
         self.open_groups_page(driver)
-        self.create_group(driver, name="", header="", footer="")
+        self.create_group(driver, Group(name="", header="", footer=""))
         self.return_to_groups_page(driver)
         self.logout(driver)
 
@@ -40,17 +41,17 @@ class test_add_group(unittest.TestCase):
     def return_to_groups_page(self, driver):
         driver.find_element(By.LINK_TEXT, value="group page").click()
 
-    def create_group(self, driver, name, header, footer):
+    def create_group(self, driver, group):
         driver.find_element(By.NAME, value="new").click()
         driver.find_element(By.NAME, value="group_name").click()
         driver.find_element(By.NAME, value="group_name").clear()
-        driver.find_element(By.NAME, value="group_name").send_keys(name)
+        driver.find_element(By.NAME, value="group_name").send_keys(group.name)
         driver.find_element(By.NAME, value="group_header").click()
         driver.find_element(By.NAME, value="group_header").clear()
-        driver.find_element(By.NAME, value="group_header").send_keys(header)
+        driver.find_element(By.NAME, value="group_header").send_keys(group.header)
         driver.find_element(By.NAME, value="group_footer").click()
         driver.find_element(By.NAME, value="group_footer").clear()
-        driver.find_element(By.NAME, value="group_footer").send_keys(footer)
+        driver.find_element(By.NAME, value="group_footer").send_keys(group.footer)
         driver.find_element(By.NAME, value="submit").click()
 
     def open_groups_page(self, driver):
