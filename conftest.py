0618e92c89acd7a98e7597138b59f73d4b3fb076
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-17 20:38:28 +0200

    3.2  Updated conftest to: put login and logout for all tests

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
-
 import pytest
 
 from fixture.application import Application
 
-@pytest.fixture(scope = "session")
+
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    yield fixture
+    fixture.session.login(username="admin", password="secret")
+
+    def fin():
+        fixture.session.logout()
+        fixture.destroy()
+
+    request.addfinalizer(fin)
+    return fixture
