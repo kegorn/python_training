f1df8c09c26495d00367249540b8392017f1f1cb
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-17 21:04:05 +0200

    3.2  Create fixture if None and check is fixture valid

--- fixture/application.py
+++ fixture/application.py
@@ -17,6 +17,13 @@ class Application:
         # self.verificationErrors = []
         # self.accept_next_alert = True
 
+    def is_valid(self):
+        try:
+            self.driver.current_url
+            return True
+        except:
+            return False
+
     def open_home_page(self):
         driver = self.driver
         driver.get("http://localhost/addressbook/index.php")
