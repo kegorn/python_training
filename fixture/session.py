f90d02c8bdfff51ff477902f40ea0b072b7c639e
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-19 18:13:55 +0200

    3.3 Add verificationі for login logout

--- fixture/session.py
+++ fixture/session.py
@@ -19,3 +19,25 @@ class SessionHelper():
     def logout(self):
         driver = self.app.driver
         driver.find_element(By.LINK_TEXT, value="Logout").click()
+
+    def ensure_logout(self):
+        # check that logout link is present
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        driver = self.app.driver
+        return len(driver.find_elements(By.LINK_TEXT, value="Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        driver = self.app.driver
+        return driver.find_element(By.XPATH, value="//div/div[1]/form/b").text == f"({username})"
+
+    def ensure_login(self, username, password):
+        driver = self.app.driver
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username=username, password=password)
