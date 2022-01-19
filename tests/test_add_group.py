2eb61f6afe6f95c26de30fc8bb731abc9c1617ca
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-19 18:13:00 +0200

    3.3 - Logout during test to brake finalizer and execution of a new tests

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -4,6 +4,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.groupHelper.create(Group(name="new_group1", header="new_group1", footer="new_group1"))
+    app.session.logout()
 
 
 def test_add_empty_group(app):
