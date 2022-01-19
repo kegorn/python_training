4677ecd54173b3192bd3a490f82b417724ea52f2
kegorn <45860085+kegorn@users.noreply.github.com>
2022-01-19 19:05:29 +0200

    3.4 Precondition: Create a group to delete if groups are absent

--- tests/test_delete_group.py
+++ tests/test_delete_group.py
@@ -1,3 +1,13 @@
 
+from model.group import Group
+
+
 def test_delete_first_group(app):
+    if app.groupHelper.count_groups() == 0:
+        app.groupHelper.create(Group(name="Group_to_delete"))
     app.groupHelper.delete_first_group()
+
+
+# def test_delete_all_groups(app):
+#     while app.groupHelper.count_groups() > 0:
+#         app.groupHelper.delete_first_group()
